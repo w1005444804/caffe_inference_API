28c98cd0a9f981148f07acc07677d2893cb5a9e9
w1005444804 <wujie5376.08@163.com>
2018-12-26 11:15:55 +0800

    Add files via upload

---
+++ excute.cpp
@@ -0,0 +1,72 @@
+#include "mini_caffe.h"
+
+InferNet::InferNet(const IFparams& ifparams){
+	Init(ifparams);
+}
+
+void InferNet::Init(const IFparams& ifparams){
+	//create net
+	EnginNetPtr_.reset(new caffe::Net(ifparams.Proto_Path));
+	EnginNetPtr_->CopyTrainedLayersFrom(ifparams.Model_Path);
+	CPU_ONLY_ = ifparams.CPU_ONLY;
+	if (CPU_ONLY_){
+		caffe::SetMode(caffe::CPU, 0);
+	}
+	else {
+		caffe::SetMode(caffe::GPU, 0);
+	}
+
+	for (int i = 0; i < ifparams.object_names.size(); ++i){
+		std::string name = ifparams.object_names[i];
+		std::shared_ptr<caffe::Blob> blob = EnginNetPtr_->blob_by_name(name);
+		EnginNetPtr_->MarkOutputs({name});
+		if (OutPut_.count(name) == 0){
+			std::shared_ptr<Datum> datum;
+			BlobToDatum(blob, datum);
+			OutPut_.insert(std::make_pair(name, datum));
+		}
+		else {
+			printf("the object name: %s has defined: \n", name);
+			CHECK(0);
+		}
+	}
+}
+
+void InferNet::Infer(const std::shared_ptr<Datum> input_datum){
+	//input blob name is data;
+	DatumToBlob(input_datum, EnginNetPtr_->blob_by_name("data"));
+
+	// it is a bug when this dll is used to compile other dll.
+	if (CPU_ONLY_){
+		caffe::SetMode(caffe::CPU, 0);
+	} else {
+		caffe::SetMode(caffe::GPU, 0);
+	}
+
+	EnginNetPtr_->Forward();
+
+	//configure output
+	for (auto it = OutPut_.begin(); it != OutPut_.end(); ++it){
+		std::string name = it->first;
+		std::shared_ptr<Datum> datum = it->second;
+		std::shared_ptr<caffe::Blob> blob = EnginNetPtr_->blob_by_name(name);
+		BlobToDatum(blob, datum);
+	}
+}
+/* ------------------------------------------------- */
+/* ------------------------------------------------- */
+void Inference::Init(IFparams& pParams){
+	if (InferNet_ != nullptr){
+		delete static_cast<InferNet*>(InferNet_);
+	}
+	InferNet* IFN = new InferNet();
+	IFN->Init(pParams);
+	InferNet_ = static_cast<void*>(IFN);
+}
+Inference::IFReult Inference::Infer(const std::shared_ptr<Datum> input_datum){
+	static_cast<InferNet*>(InferNet_)->Infer(input_datum);
+	return static_cast<InferNet*>(InferNet_)->GetResult();
+}
+Inference::~Inference(){
+	delete static_cast<InferNet*>(InferNet_);
+}
---
+++ mini_caffe.h
@@ -0,0 +1,82 @@
+#ifndef  _MINI_CAFFE_H_
+#define  _MINI_CAFFE_H_
+#pragma once
+
+#include "Infer.h"
+
+#include "caffe/caffe.hpp"
+#include "caffe/c_api.h"
+
+#include <fstream>
+
+using SizeVector = std::vector<size_t>;
+
+#define CHECK_SUCCESS(condition)                  \
+  if ((condition) != 0) {                         \
+    printf("CHECK (" #condition ") failed\n");    \
+    printf("%s\n", CaffeGetLastError());          \
+    exit(-1);                                     \
+      }
+
+#define CHECK(condition)                          \
+  if (!(condition)) {                             \
+    printf("CHECK (" #condition ") failed\n");    \
+    exit(-1);                                     \
+      }
+
+bool compare_shape(const Shape& S1, const Shape& S2) {
+	if (S1.W != S2.W || S1.H != S2.H ||
+		S1.C != S2.C || S1.N != S2.N) {
+		return 1;
+	}
+	return 0;
+}
+
+void DatumToBlob(const std::shared_ptr<Datum> datum, std::shared_ptr<caffe::Blob> blobptr){
+	std::vector<int> shape;
+	shape.reserve(4);
+	shape.push_back(datum->shape.N);
+	shape.push_back(datum->shape.C);
+	shape.push_back(datum->shape.H);
+	shape.push_back(datum->shape.W);
+	blobptr->Reshape(shape);
+	float* blob_data = blobptr->mutable_cpu_data();
+	std::memcpy(blob_data, datum->Getdata(), datum->shape.count() * sizeof(float));
+}
+
+void BlobToDatum(const std::shared_ptr<caffe::Blob> blobptr, std::shared_ptr<Datum> datum){
+	const std::vector<int> shape = blobptr->shape();
+	Shape newdatumshape;
+	newdatumshape.N = shape[0];
+	newdatumshape.C = shape[1];
+	newdatumshape.H = shape[2];
+	newdatumshape.W = shape[3];
+	datum->out_shape = newdatumshape;
+	datum->outter_data = blobptr->mutable_cpu_data();
+}
+
+class InferNet {
+public:
+	explicit InferNet(const IFparams& ifparam);
+
+	InferNet(){};
+
+	virtual~InferNet(){};
+
+	void Init(const IFparams& ifparams);
+
+	void Infer(const std::shared_ptr<Datum> input_datum);
+
+	Inference::IFReult GetResult(){ return OutPut_; };
+
+private:
+
+	std::shared_ptr<caffe::Net> EnginNetPtr_;
+
+	Inference::IFReult OutPut_;
+
+	bool CPU_ONLY_;
+};
+
+
+#endif //_MTCNN_H_
