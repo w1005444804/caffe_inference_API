b2a825f305f945cfe19669e23e85cef43ccd3de2
w1005444804 <wujie5376.08@163.com>
2018-12-26 11:16:55 +0800

    Add files via upload

---
+++ Infer.h
@@ -0,0 +1,91 @@
+#ifndef  _TNT_H_
+#define  _TNT_H_
+
+#include <memory>
+#include <map>
+#include <vector>
+
+#ifdef _WIN32
+#ifdef INFER_EXPORTS
+#define INFER_API __declspec(dllexport)
+#else
+#define INFER_API __declspec(dllimport)
+#endif
+//added else for support Unix
+#else
+#define RPN_API
+#endif
+//#define CPU_ONLY
+
+
+struct Shape {
+	unsigned int N{ 0 };
+	unsigned int C{ 0 };
+	unsigned int H{ 0 };
+	unsigned int W{ 0 };
+	unsigned int count() {
+		return N*C*H*W;
+	}
+};
+
+struct Datum {
+	Shape shape;
+	float* data = nullptr;//NCHW
+	Shape out_shape;
+	float* outter_data = nullptr;
+
+	Datum() {
+		data = nullptr;
+	}
+	float* Getdata() {
+		if (data != nullptr) {
+			return data;
+		}
+		else {
+			unsigned int size = shape.count();
+			data = new float[size];
+			return data;
+		}
+	}
+	void Reshape(Shape s) {
+		if (data != nullptr && s.count() > shape.count()) {
+			delete[] data;
+			data = nullptr;
+		}
+		shape = s;
+	}
+	~Datum() {
+		if (data != nullptr){
+			delete[] data;
+			data = nullptr;
+		}
+	}
+};
+
+struct IFparams {
+	bool CPU_ONLY{ 1 };
+	char* Root_Path;
+	char* Model_Path;
+	char* Proto_Path;
+	Shape shape;
+	std::vector<std::string> object_names;
+};
+
+class INFER_API Inference {
+public:
+	typedef std::map<std::string, std::shared_ptr<Datum>> IFReult;
+
+	Inference(){};
+
+	virtual~Inference();
+
+	void Init(IFparams& ifparams);
+
+	IFReult Infer(const std::shared_ptr<Datum> input_datum);
+
+private:
+
+	void* InferNet_ = nullptr;
+
+};
+#endif //_MTCNN_H_
